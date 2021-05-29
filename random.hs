98502b442f870d76b0e2b43c5a7153091b96bd9f
Tom Harke <tom.harke@gmail.com>
2021-05-28 19:22:55 -0700

    eg code snippet using Crypto.Hash
    Taken from:
            https://www.fpcomplete.com/blog/2017/09/cryptographic-hashing-haskell/

--- random.hs
+++ random.hs
@@ -1,3 +1,21 @@
+
+import           Crypto.Hash             (hash, SHA256 (..), Digest)
+import           Data.ByteString         (ByteString)
+import           Data.Text.Encoding      (encodeUtf8)
+import qualified Data.Text.IO            as TIO
+import           System.IO               (hFlush, stdout)
+
+main :: IO ()
+main = do
+  putStr "Enter some text: "
+  hFlush stdout
+  text <- TIO.getLine
+  let bs = encodeUtf8 text
+  putStrLn $ "You entered: " ++ show bs
+  let digest :: Digest SHA256
+      digest = hash bs
+  putStrLn $ "SHA256 hash: " ++ show digest
+
 {- pseudocode from chapter 9 of
       title: Design Principles and Practical Applications
       author:
@@ -6,7 +24,6 @@
          - Tadayoshi Kohno
       isbn: 978-0-470-47424-2
 -}
-
 
 
 {-
